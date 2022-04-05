deb56b02507ca84fbd7f2180d544e1548b421b59
y.bakhar <egorbahar@gmail.com>
2022-04-05 15:56:11 +0300

    Added User and Role entities

---
+++ src/main/java/com/godeltech/persistence/model/User.java
@@ -0,0 +1,21 @@
+package com.godeltech.persistence.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+}
