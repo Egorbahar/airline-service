1d3f0bc358694fa4bfee64a1f69511d491320ccc
y.bakhar <egorbahar@gmail.com>
2022-04-15 14:17:17 +0300

    Refactor code

--- src/main/java/com/godeltech/persistence/repository/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.godeltech.persistence.repository;
-
-import com.godeltech.persistence.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City, Long> {
-}
--- src/main/java/com/godeltech/persistence/repository/EquipageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.godeltech.persistence.repository;
-
-import com.godeltech.persistence.model.Equipage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipageRepository extends JpaRepository<Equipage,Long> {
-}
