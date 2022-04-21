3a69171166cdbfeba7a82ee96f9abb903589d80e
y.bakhar <egorbahar@gmail.com>
2022-04-21 16:04:13 +0300

    Refactor code

--- src/main/java/com/godeltech/web/dto/request/UserRequestDto.java
+++ src/main/java/com/godeltech/web/dto/request/UserRequestDto.java
@@ -2,9 +2,15 @@ package com.godeltech.web.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class UserRequestDto {
+    @NotBlank(message = "{user.username.notBlank}")
     private final String username;
+    @NotBlank(message = "{user.password.notBlank}")
     private final String password;
+    @NotNull(message = "{user.roleId.notNull}")
     private final Long roleId;
 }
